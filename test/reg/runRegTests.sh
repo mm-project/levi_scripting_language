85ba7f66569f29db3a8d3acdf2c43b772449500d
levibyte <kartol@gmx.com>
2018-02-14 13:24:13 +0400

    complete: reg test runner is running :)))

--- test/reg/runRegTests.sh
+++ test/reg/runRegTests.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
-$levi_exe=../../bin/levi
+mode=$1
 
-for fn in `ls data`; do
+#FIXME
+path=/c/Users/levons/Desktop/git/diploma_levibyte/diploma
+out_dir=./reg_output
+
+levi_exe=$path/bin/levi.exe
+data_dir=$path/test/reg/data
+golden_dir=$path/test/reg/golden
+
+
+
+mkdir -p $out_dir
+for fn in `ls $data_dir`; do
 	testname=`echo $fn| cut -d'.' -f1`
-    $levi_exe -i data/$fn > output
-	a=`diff output ...` 
+	echo  -ne "		Running test <$testname> : "
+    $levi_exe $data_dir/$fn > $out_dir/$testname.out
+	if [ "$mode" == "" ]; then
+		result=`diff $golden_dir/$testname.out $out_dir/$testname.out &> /dev/null` 
+		if [ "$?" != 0 ] || [ "$result" != "" ]; then
+			echo "  Fail"
+		else
+			echo  "	Pass "
 		
-	echo -ne "Test $testname "
-    
-	cat $fn
-	echo Pass
+		fi
+	elif [ "$mode" == "regolden" ]; then
+		cp $out_dir/$testname.out $golden_dir/$testname.out
+	fi
 done
+
+rm -rf $out_dir
