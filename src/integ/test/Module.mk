0e80142f8fbc4ab619b6d8999adb5f79be511283
levibyte <kartol@gmx.com>
2018-04-09 00:28:01 +0400

    fix travis failure

--- src/integ/test/Module.mk
+++ src/integ/test/Module.mk
@@ -1,4 +1,5 @@
 LOCAL_INCS_FIXME:=-I include/integ
-LOCAL_LIBS_FIXME:=-L $(LIB_DIR) $(LIB_DIR)/libLeviInterp.so
+LOCAL_LIBS_FIXME:=-L $(LIB_DIR) -lLeviInterp
+#LOCAL_LIBS_FIXME:=-L $(LIB_DIR) $(LIB_DIR)/libLeviInterp.so
 
 include $(RULES_DIR)/unit_test.rl
